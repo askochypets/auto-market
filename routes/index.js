2719a10c16dddca44f33f806343f8640a7a2f3fc
Andiy Skochypets <askochypets@ancestry.com>
2015-10-25 14:24:40 +0300

    Added detail pages.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fiat', function(req, res, next) {
+  res.render('fiat', { title: 'Fiat' });
+});
+
+router.get('/citroen', function(req, res, next) {
+  res.render('citroen', { title: 'Citroen' });
+});
+
+router.get('/ford', function(req, res, next) {
+  res.render('ford', { title: 'Ford' });
+});
+
+router.get('/peugeot', function(req, res, next) {
+  res.render('peugeot', { title: 'Peugeot' });
+});
+
+router.get('/renault', function(req, res, next) {
+  res.render('renault', { title: 'Renault' });
+});
+
+router.get('/mercedes', function(req, res, next) {
+  res.render('mercedes', { title: 'Mercedes' });
+});
+
 module.exports = router;
