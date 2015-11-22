3302c2fe56c3635bad1db483c1c83385de47833f
Andiy Skochypets <askochypets@ancestry.com>
2015-11-22 03:46:26 +0200

    new DB

--- DB/Dump20151122.sql
+++ DB/Dump20151122.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `automarket` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `automarket`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: localhost    Database: automarket
+-- Host: 127.0.0.1    Database: automarket
 -- ------------------------------------------------------
--- Server version	5.7.9-log
+-- Server version	5.7.9
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,6 +27,7 @@ DROP TABLE IF EXISTS `auto`;
 CREATE TABLE `auto` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
+  `imgurl` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UX_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
@@ -36,7 +39,7 @@ CREATE TABLE `auto` (
 
 LOCK TABLES `auto` WRITE;
 /*!40000 ALTER TABLE `auto` DISABLE KEYS */;
-INSERT INTO `auto` VALUES (8,'Citroen'),(9,'Fiat'),(1,'Ford'),(7,'Mercedes'),(10,'Nissan'),(6,'Opel'),(11,'Peugeot'),(12,'Renault'),(13,'Volkswagen');
+INSERT INTO `auto` VALUES (1,'Ford','/images/mark/ford.png'),(6,'Opel','/images/mark/opel.png'),(7,'Mercedes','/images/mark/mercedes.png'),(8,'Citroen','/images/mark/citroen.png'),(9,'Fiat','/images/mark/fiat.png'),(10,'Nissan','/images/mark/nissan.png'),(11,'Peugeot','/images/mark/peugeot.png'),(12,'Renault','/images/mark/renault.png'),(13,'Volkswagen','/images/mark/volkswagen.png');
 /*!40000 ALTER TABLE `auto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +58,7 @@ CREATE TABLE `models` (
   UNIQUE KEY `UX_name` (`name`),
   KEY `FK_auto` (`auto_id`),
   CONSTRAINT `models_ibfk_1` FOREIGN KEY (`auto_id`) REFERENCES `auto` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +70,34 @@ LOCK TABLES `models` WRITE;
 INSERT INTO `models` VALUES (1,'Connect',1),(2,'Courier',1),(4,'Transit',1),(5,'Movano',6),(6,'Vivaro',6),(7,'Berlingo',8),(8,'Jumper',8),(9,'Jumpy',8),(10,'Nemo',8),(11,'Doblo',9),(12,'Ducato',9),(13,'Scudo',9),(14,'Sprinter',7),(15,'Vito',7),(16,'Kubistar',10),(17,'Primastar',10),(18,'Bipper',11),(19,'Boxer',11),(20,'Expert',11),(21,'Partner',11),(22,'Kengoo',12),(23,'Mascott',12),(24,'Master',12),(25,'Trafic',12),(26,'Caddy',13),(27,'Crafter',13);
 /*!40000 ALTER TABLE `models` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `parts`
+--
+
+DROP TABLE IF EXISTS `parts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `model_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UX_name` (`name`),
+  KEY `FK_model` (`model_id`),
+  CONSTRAINT `parts_ibfk_1` FOREIGN KEY (`model_id`) REFERENCES `models` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parts`
+--
+
+LOCK TABLES `parts` WRITE;
+/*!40000 ALTER TABLE `parts` DISABLE KEYS */;
+INSERT INTO `parts` VALUES (1,'Engine',1),(2,'Transmission',1),(3,'Camshaft',1);
+/*!40000 ALTER TABLE `parts` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -77,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-20 19:08:51
+-- Dump completed on 2015-11-22  3:45:28
