0c165a2a49d9efa81e0c9e2cc994a3c24aa5d493
Andiy Skochypets <askochypets@ancestry.com>
2015-11-22 03:16:09 +0200

    Implemented DB functionality

--- DB/insert.sql
+++ DB/insert.sql
@@ -1,3 +1,5 @@
 INSERT auto (name) VALUES ('test');
 
 INSERT models (name, auto_id) VALUES ("Focus", 1);
+
+DELETE FROM models WHERE name="Focus";
--- routes/index.js
+++ routes/index.js
@@ -12,28 +12,90 @@ var express = require('express'),
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Auto list' });
+router.get('/', function(req, res, next) {
+    connection.query('SELECT a.name maker, a.imgurl imgurl, m.name model FROM models m INNER JOIN auto a ON m.auto_id = a.id;', function(err, rows) {
+        var list = {};
+        if (err) throw err;
+
+        //create json
+        for (obj in rows){
+            if (!list[rows[obj].maker]){
+                list[rows[obj].maker] = {imgurl: '', models: []};
+            }
+            list[rows[obj].maker].imgurl = rows[obj].imgurl;
+            list[rows[obj].maker].models.push(rows[obj].model);
+        }
+
+        //render view
+        res.render('index', { title: 'List of Auto', list: list });
+    });
 });
 
-/* GET other page. */
+/* GET all models page. */
 
 router.get('/models', function(req, res) {
     connection.query('SELECT a.name maker, m.name model FROM models m INNER JOIN auto a ON m.auto_id = a.id;', function(err, rows) {
-        if (err) throw err;
         var list = {};
 
-
-        for (var obj in rows) {
-            
+        if (err) throw err;
+        //create json
+        for (obj in rows){
+            if (!list[rows[obj].maker]){
+                list[rows[obj].maker] = [];
+            }
+            list[rows[obj].maker].push(rows[obj].model);
         }
-
+        console.log(list);
+        //render view
+        res.render('models', { list: list });
     });
-    connection.end();
-    res.render('models', { title: 'mercedes benz' });
+});
+
+module.exports = router;
 
 
+/* GET exact maker page. */
 
+router.get('/:maker', function(req, res, next) {
+    connection.query('SELECT a.name maker, m.name model FROM models m INNER JOIN auto a ON m.auto_id = a.id WHERE a.name ="' + req.params.maker + '"', function(err, rows) {
+        var list = {};
+
+        if (err) throw err;
+        //create json
+        for (obj in rows){
+            if (!list[rows[obj].maker]){
+                list[rows[obj].maker] = [];
+            }
+            list[rows[obj].maker].push(rows[obj].model);
+        }
+        if (JSON.stringify(list) !== "{}") {
+            //render view
+            res.render('models', { title: req.params.maker + ' Models', list: list });
+        } else {
+            next();
+        }
+    });
+});
+
+/* GET exact model page. */
+
+router.get('/:maker/:model', function(req, res, next) {
+    connection.query('SELECT a.name maker, m.name model FROM models m INNER JOIN auto a ON m.auto_id = a.id WHERE a.name ="' + req.params.maker + '" AND m.name ="' + req.params.model + '"', function(err, rows) {
+        var list = {};
+
+        if (err) throw err;
+        //create json
+        list.maker = rows[0].maker;
+        list.model = rows[0].model;
+
+        console.log(list);
+        if (JSON.stringify(list) !== "{}") {
+            //render view
+            res.render('parts', { title: req.params.maker + " " + req.params.model, list: list });
+        } else {
+            next();
+        }
+    });
 });
 
 module.exports = router;
