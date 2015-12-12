48ca7565019b87eb20a6aaa5a65bad11cf09b6c8
Andriy Skochypets <askochypets@ancestry.com>
2015-12-13 00:50:09 +0200

    Remove needless files.

--- DB/insert.sql
+++ DB/insert.sql
@@ -3,3 +3,7 @@ INSERT auto (name) VALUES ('test');
 INSERT models (name, auto_id) VALUES ("Focus", 1);
 
 DELETE FROM models WHERE name="Focus";
+
+ALTER TABLE models ADD imgurl VARCHAR(255) NULL;
+
+ALTER TABLE models DROP COLUMN imgurl;
